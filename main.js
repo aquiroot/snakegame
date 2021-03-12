602800f2b70da91ddb327c7ab9ea29e3d2817e3a
Damian Colaneri <dcolaneri@gmail.com>
2021-03-11 22:26:24 -0300

    [ADD] movimiento

--- main.js
+++ main.js
@@ -1,69 +1,61 @@
-/**
- * PARAMETROS DE IMAGEN
- */
-
 // configuracion de canvas
-const gameBoard = document.getElementById('game__board');
-const gameBoard_ctx = game__board.getContext('2d');
+const board = document.querySelector('canvas');
+const boardCtx = board.getContext('2d');
 
-// Inicio del juego
-main();
+// variables
+const direction = {
+	s: [0, 1],
+	w: [0, -1],
+	d: [1, 0],
+	a: [-1, 0],
+	S: [0, 1],
+	W: [0, -1],
+	D: [1, 0],
+	A: [-1, 0],
+	ArrowDown: [0, 1],
+	ArrowUp: [0, -1],
+	ArrowRight: [1, 0],
+	ArrowLeft: [-1, 0],
+};
 
-// Posicion inicial de la serpiente
-let snake = [
-	{ x: 200, y: 200 },
-	{ x: 190, y: 200 },
-	{ x: 180, y: 200 },
-	{ x: 170, y: 200 },
-	{ x: 160, y: 200 },
-];
+let keyPress;
+const intervalo = 80;
+const speed = 10;
 
-// Varibles de color de elementos del juego
-const board_border = 'black';
-const board_background = 'white';
-const snake_col = 'lightblue';
-const snake_border = 'darkblue';
+const controls = {
+	direction: { x: 1 * speed, y: 0 },
+	serpiente: [{ x: 300, y: 300 }],
+};
 
-// Crea y rellena los bordes al canvas
-function clearCanvas() {
-	gameBoard_ctx.fillStyle = board_background;
-	gameBoard_ctx.strokestyle = board_border;
-	gameBoard_ctx.fillRect(0, 0, gameBoard.width, gameBoard.height);
-	gameBoard_ctx.strokeRect(0, 0, gameBoard.width, gameBoard.height);
-}
+// Obtener pulsaciones de teclas
+document.onkeydown = (ev) => {
+	keyPress = direction[ev.key];
+	const [x, y] = keyPress;
+	if (-x !== controls.direction.x && -y !== controls.direction.y) {
+		controls.direction.x = x * speed;
+		controls.direction.y = y * speed;
+	}
+};
 
-function drawSnakePart(snakePart) {
-	gameBoard_ctx.fillStyle = 'lightblue';
-	gameBoard_ctx.strokestyle = 'darkblue';
-	gameBoard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-	gameBoard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-}
+const draw = (color) => {
+	boardCtx.clearRect(0, 0, 600, 600);
+	boardCtx.fillStyle = 'green';
+	const head = controls.serpiente[0];
+	boardCtx.fillRect(head.x, head.y, 10, 10);
+	console.log(head);
+};
 
-// Funcion para dibujar las partes de la serpiente
-function drawSnake() {
-	snake.forEach(drawSnakePart);
-}
+// funcion start
+const start = () => {
+	const head = controls.serpiente[0];
+	let dx = controls.direction.x;
+	let dy = controls.direction.y;
+	head.x += dx;
+	head.y += dy;
+	requestAnimationFrame(draw);
+	setTimeout(start, intervalo);
+};
 
-/**
- * PARAMETROS DE MOVIMIENTO
- */
-
-function move_snake() {
-	const head = { x: snake[0].x + dx, y: snake[0].y };
-	snake.unshift(head);
-	snake.pop();
-}
-
-/**
- * FUNCION DE REPETICION
- */
-
-function main() {
-	setTimeout(function onTick() {
-		clear_board();
-		move_snake();
-		drawSnake();
-		// Genera un loop de si mismo
-		main();
-	}, 100);
-}
+window.onload = () => {
+	start();
+};
