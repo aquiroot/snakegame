a0f065705c0bdf013e674c5165f09fa2c7e2f7b1
Damian Colaneri <dcolaneri@gmail.com>
2021-03-18 18:58:09 -0300

    [ADD] snake length

--- main.js
+++ main.js
@@ -36,6 +36,7 @@ const controls = {
 	serpiente: [{ x: 310, y: 180 }],
 	alimento: { x: 100, y: 100 },
 	start: false,
+	crecimiento: 0,
 };
 
 // Obtener pulsaciones de teclas
@@ -51,9 +52,13 @@ document.onkeydown = (ev) => {
 // Dibujar objetos
 const draw = () => {
 	boardCtx.clearRect(0, 0, 640, 370);
-	const head = controls.serpiente[0];
+	//const head = controls.serpiente[0];
+	for (let idx = 0; idx < controls.serpiente.length; idx++) {
+		const { x, y } = controls.serpiente[idx];
+		drawFigures('#1f3000', x, y);
+	}
 	const alimento = controls.alimento;
-	drawFigures('black', head.x, head.y);
+	//drawFigures('black', head.x, head.y);
 	drawFigures('#1f3000', alimento.x, alimento.y);
 };
 
@@ -64,26 +69,46 @@ const drawFigures = (color, x, y) => {
 
 // funcion start
 const start = () => {
+	let tail = {};
+	Object.assign(tail, controls.serpiente[controls.serpiente.length - 1]);
 	const head = controls.serpiente[0];
 	let dx = controls.direction.x;
 	let dy = controls.direction.y;
-	head.x += dx;
-	head.y += dy;
+	let size = controls.serpiente.length - 1;
+	for (let idx = size; idx > -1; idx--) {
+		let head = controls.serpiente[idx];
+		if (idx === 0) {
+			head.x += dx;
+			head.y += dy;
+		} else {
+			head.x = controls.serpiente[idx - 1].x;
+			head.y = controls.serpiente[idx - 1].y;
+		}
+	}
+
 	if (
 		head.x < -1 ||
 		head.y < -1 ||
 		head.x > boardWidth / 10 ||
 		head.y > boardHeight / 10
 	) {
-		window.alert('looooser!!!');
+		window.alert('ha perdido, puntaje: ' + score * speed);
 		location.reload();
+		return;
 	}
 
 	if (head.x === controls.alimento.x && head.y === controls.alimento.y) {
+		controls.crecimiento += 1;
 		eaten();
 		score++;
 		scoreScreen.innerHTML = score * speed;
 	}
+
+	if (controls.crecimiento > 0) {
+		controls.serpiente.push(tail);
+		controls.crecimiento = 0;
+	}
+	//console.log(controls.serpiente);
 	requestAnimationFrame(draw);
 	setTimeout(start, intervalo);
 };
